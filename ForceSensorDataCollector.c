fa4f1ae4c4a9eb098445d3ab1930c60e0006b008
Alexander Infantino <infantinoalex@gmail.com>
2018-07-25 21:32:25 -0400

    Spawned emergency stops on child processes

--- ForceSensorDataCollector.c
+++ ForceSensorDataCollector.c
@@ -17,19 +17,30 @@
  * 
  */
 
+#include <stdlib.h>
 #include <stdio.h>
-#include <conio.h>
 #include <unistd.h>
+#include <time.h>
 
-float GetForceSensorValue(int motor);
-int EmergencyStopHit();
+// If a signal is received in the parent, it will safely close the files before terminating
+void sig_handler(int signal);
+
+// Grabs the reading off of a force sensor
+float GetForceSensorValue(int sensor_key);
+
+// If a key is pressed, it will trigger a signal to kill the parent. Used as an emergency stop
+// This will be constantly running on a child process so as to not miss any key that may be pressed.
+int EmergencyStopHitViaKeyPress(pid_t parent_id);
+
+// If the physical emergency stop device is triggered, it will send a signal to kill the parent.
+// This will be constantly running on a child process so as to not miss if the stop is triggered.
+int EmergencyStopHitViaPhysicalDevice(pid_t parent_id);
+
+// Writes the given data to the given file
 void WriteDataToFile(float sensorData, FILE * file);
 
 FILE * rightSensorLogFile;
 FILE * leftSensorLogFile;
-
-const int RightForceSensor = 0;
-const int LeftForceSensor = 1;
 
 int main(int argc, char ** argv)
 {
@@ -45,6 +56,47 @@ int main(int argc, char ** argv)
     rightSensorLogFile = fopen(rightSensorFileName, "w");
     leftSensorLogFile = fopen(leftSensorFileName, "w");
 
+    // Setting up Signal Handlers
+    // When the child detects an emergency stop, it will send the signal which will be caught to properly clean up environment before terminating.
+    struct sigaction new_action;
+    sigset_t mask_sigs;
+    int nsigs;
+    int sigs[] = {SIGHUP, SIGINT, SIGQUIT, SIGBUS, SIGTERM, SIGSEGV, SIGFPE};
+    nsigs = sizeof(sigs) / sizeof(int);
+    sigemptyset(&mask_sigs);
+
+    for (i = 0; i < nsigs; ++i)
+    {
+        new_act.sa_handler = sig_handler;
+        new_act.sa_mask = mask_sigs;
+        new_act.sa_flags = 0;
+
+        if (sigaction(sigs[i], &new_act, NULL) == -1)
+        {
+            perror("Can't set signal handlers.");
+            exit(1);
+        }
+    }
+
+    // Spawning two child processes to constantly poll for emergency stop
+    pid_t parent_id = getpid();
+    pid_t pid;
+
+    if ((pid = fork())== 0)
+    {
+        // First child which will monitor key press
+        EmergencyStopHitCode(parent_id);
+        exit(2);
+    }
+
+    if ((pid = fork()) ==)
+    {
+        // Second child which will monitor physical emergency stop
+        EmergencyStopHitStrap(parent_id);
+        exit(3);
+    }
+
+    // In parent code
     if (rightSensorLogFile == NULL)
     {
         printf("Failed to open log file of name: %s.\nCannot record data.\n", rightSensorFileName);
@@ -58,14 +110,15 @@ int main(int argc, char ** argv)
     printf("Successfully open log file to record data at: %s\n", rightSensorFileName);
     printf("Successfully open log file to record data at: %s\n", leftSensorFileName);
 
+    const int RightForceSensor = 0;
+    const int LeftForceSensor = 1;
+
+    struct timespec sleepTime;
+    sleepTime.tv_sec = 0;
+    sleepTime.tv_nsec = 100000000;
+
     while (1)
     {
-        if (EmergencyStopHit())
-        {
-            printf("\n\n\nEMERGENCY STOP HAS BEEN HIT.\n\n\nExiting program and performing cleanup.\n");
-            break;
-        }
-
         float rightForceSensorValue = GetForceSensorValue(RightForceSensor);
         float leftForceSensorValue = GetForceSensorValue(LeftForceSensor);
         
@@ -78,8 +131,8 @@ int main(int argc, char ** argv)
         WriteDataToFile(rightForceSensorValue, rightSensorLogFile);
         WriteDataToFile(leftForceSensorValue, leftSensorLogFile);
 
-        // Sleeps for 1 second. That might be too slow. Need to look at other sleep functions in different libraries/how to use them.
-        sleep(1);
+        // Sleeps for .1 second.
+        nanosleep(sleepTime);
     }
 
     if (fclose(rightSensorLogFile) != 0)
@@ -95,15 +148,31 @@ int main(int argc, char ** argv)
     return 0;
 }
 
-float GetForceSensorValue(int motor)
+void sig_handler(int signal)
 {
-    if (motor == RightForceSensor)
+    printf("\n\n\nEmergency Stop Activated!\n\nExiting Program\n");
+    if (fclose(rightSensorLogFile) != 0)
+    {
+        printf("Could not close file properly: %s\n", rightSensorFileName);
+    }
+
+    if (fclose(leftSensorLogFile) != 0)
+    {
+        printf("Could not close file properly: %s\n", leftSensorFileName);
+    }
+
+    exit(5);
+}
+
+float GetForceSensorValue(int sensor_key)
+{
+    if (sensor_key == RightForceSensor)
     {
         // Poll Right Force Sensor
         return -1;
     }
 
-    if (motor == LeftForceSensor)
+    if (sensor_key == LeftForceSensor)
     {
         // Poll Left Force Sensor
         return -1;
@@ -113,15 +182,42 @@ float GetForceSensorValue(int motor)
     return -1;
 }
 
-int EmergencyStopHit()
+void EmergencyStopHitViaKeyPress(pid_t parent_id)
 {
-    // If a key has been pressed
-    if (kbhit())
+    while (1)
     {
-        return 1;
-    }
+        if (kbhit())
+        {
+            // Send the kill signal
+            kill(parent_id, SIGINT);
+        }
 
-    return 0;
+        pid_t parent_check = getppid();
+        if (parent_check == 1)
+        {
+            // Parent has been killed. Exit
+            exit(5);
+        }
+    }
+}
+
+void EmergencyStopHitViaPhysicalDevice(pid_t parent_id)
+{
+    while (1)
+    {
+        // TODO: Determine how to check if physical emergency stop is hit
+        if (0)
+        {
+            kill(parent_id, SIGINT);
+        }
+
+        pid_t parent_check = getppid();
+        if (parent_check == 1)
+        {
+            // Parent has been killed. Exit
+            exit(5);
+        }
+    }
 }
 
 void WriteDataToFile(float sensorData, FILE * file)
